e2477bb293fd9903047aa3913030d5fd8640cd6b
Timothy Cevallos <timcev@gmail.com>
2017-10-25 00:20:16 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,16 +4,15 @@ class Genre < ActiveRecord::Base
     has_many :songs, through: :song_genres
     has_many :artists, through: :songs
 
+    
     def slug
       self.name.downcase.split(" ").join("-")
     end
-
     def self.find_by_slug(slug)
       match = ""
-
       self.all.each do |genre|
         if genre.slug == slug
-           match = genre
+          match = genre
         end
       end
       match
