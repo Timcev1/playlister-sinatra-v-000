29fd840ad95a68d167c81c87ef59b46d11d07869
Timothy Cevallos <timcev@gmail.com>
2017-10-25 00:22:52 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,18 +1,18 @@
 class Genre < ActiveRecord::Base
-    has_many :song_genres
-    has_many :songs, through: :song_genres
-    has_many :artists, through: :songs
-    
-    def slug
-      self.name.downcase.split(" ").join("-")
-    end
-    def self.find_by_slug(slug)
-      match = ""
-      self.all.each do |genre|
-        if genre.slug == slug
-          match = genre
-        end
+  has_many :song_genres
+  has_many :songs, through: :song_genres
+  has_many :artists, through: :songs
+
+  def slug
+    self.name.downcase.split(" ").join("-")
+  end
+  def self.find_by_slug(slug)
+    match = ""
+    self.all.each do |genre|
+      if genre.slug == slug
+        match = genre
       end
-      match
     end
- end
+    match
+  end
+end
