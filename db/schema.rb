66ac304ce7a348072af77852ec4d1cd05487ec74
Timothy Cevallos <timcev@gmail.com>
2017-10-25 00:00:55 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024234007) do
+ActiveRecord::Schema.define(version: 20171024235901) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "genre", force: :cascade do |t|
+  create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
